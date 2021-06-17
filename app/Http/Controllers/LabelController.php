b4ea57df1afa730900b586a0975ee8da40308aa2
Jose Chacon <j.daniel.chg@gmail.com>
2021-06-16 21:44:55 -0400

    feat: - Create basic structure of LabelController. - Create basic structure of NoteController. - Create basic structure of LoginController.

---
+++ app/Http/Controllers/LabelController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LabelController extends Controller
+{
+     public function index()
+     {
+          //
+     }
+
+     public function addLabel($id)
+     {
+          //
+     }
+
+     public function store(Request $request)
+     {
+          //
+     }
+
+     public function update(Request $request, $id)
+     {
+          //
+     }
+
+     public function destroy($id)
+     {
+          //
+     }
+}
---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+     public function index()
+     {
+          //
+     }
+
+     public function checkLogin(Request $request)
+     {
+          //
+     }
+
+     public function register()
+     {
+          //
+     }
+
+     public function checkRegister(Request $request)
+     {
+          //
+     }
+}
